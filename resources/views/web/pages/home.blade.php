9c3cd9293f4147f1797fa543d377191285028fbd
Marcel Stadelmann <marcel@jamon.digital>
2020-12-27 15:10:07 +0100

    production build .01

--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -1,7 +1,72 @@
 @extends('web.layout.app')
 @section('seo_title', 'Home')
 @section('content')
-<section>
-
+<section class="post">
+  <div class="card">
+    <figure>
+      <img src="/assets/img/day.jpg" height="1000" width="1000" class="is-responsive">
+    </figure>
+    <footer class="card__footer">
+      <div>
+        <label>Client</label>
+        <div>Day, Zürich</div>
+      </div>
+      <div>
+        <label>Concept & Design</label>
+        <div>WBG AG – Visuelle Kommunikation, Zürich</div>
+      </div>
+    </footer>
+  </div>
+</section>
+<section class="post">
+  <div class="card">
+    <figure>
+      <img src="/assets/img/hame.jpg" height="1000" width="1000" class="is-responsive">
+    </figure>
+    <footer class="card__footer">
+      <div>
+        <label>Client</label>
+        <div>Hauser Meier Architektinnen, Zürich</div>
+      </div>
+      <div>
+        <label>Concept & Design</label>
+        <div>Bivgrafik GmbH, Zürich</div>
+      </div>
+    </footer>
+  </div>
+</section>
+<section class="post">
+  <div class="card">
+    <figure>
+      <img src="/assets/img/intercable.jpg" height="1000" width="1000" class="is-responsive">
+    </figure>
+    <footer class="card__footer">
+      <div>
+        <label>Client</label>
+        <div>Ferratec AG, Rudolfstetten</div>
+      </div>
+      <div>
+        <label>Concept & Design</label>
+        <div>marceli.to, Winterthur</div>
+      </div>
+    </footer>
+  </div>
+</section>
+<section class="post">
+  <div class="card">
+    <figure>
+      <img src="/assets/img/oxid.jpg" height="1000" width="1000" class="is-responsive">
+    </figure>
+    <footer class="card__footer">
+      <div>
+        <label>Client</label>
+        <div>Oxid Architektur GmbH, Zürich</div>
+      </div>
+      <div>
+        <label>Concept & Design</label>
+        <div>Bivgrafik GmbH, Zürich</div>
+      </div>
+    </footer>
+  </div>
 </section>
 @endsection
--- resources/views/web/partials/menu.blade.php
+++ resources/views/web/partials/menu.blade.php
@@ -1,4 +1,4 @@
-<nav class="site-menu js-menu">
+<nav class="site-menu js-menu" style="display:none">
   <ul>
     <li>
       <a href="" title=""></a>
