25ced6abbad76027d8966bd221e40dffee473cd6
Marcel Stadelmann <marcel@jamon.digital>
2021-01-04 08:07:09 +0100

    responsive images

--- resources/views/web/components/content/card.blade.php
+++ resources/views/web/components/content/card.blade.php
@@ -1,8 +1,21 @@
 <section class="post">
   <a href="{{$uri}}" target="_blank" rel="noreferrer" title="{{$website}} besuchen" class="card">
-    <figure>
+    
+    {{-- <figure>
       <img data-src="/assets/img/projects/{{$image}}" height="1000" width="1000" title="{{$client}}" class="is-responsive lazyload">
-    </figure>
+    </figure> --}}
+
+    
+    <picture>
+      <source media="(min-width: 1200px)" srcset="/assets/img/projects/{{$image}}.jpg">
+      <source media="(min-width: 768px)" srcset="/assets/img/projects/{{$image}}-md.jpg">
+      <source srcset="/assets/img/projects/{{$image}}-sm.jpg">
+      <img src="/assets/img/projects/{{$image}}-sm.jpg" alt="{{$client}}" title="{{$client}}" class="is-responsive lazyload">
+    </picture>
+
+
+
+
     <div class="card__content">
       <div class="card__description">
         <div>
