a9091425217561f913e819f46831da71143660a3
Marcel Stadelmann <marcel@jamon.digital>
2021-01-04 07:52:16 +0100

    rc 7

--- resources/views/web/pages/index.blade.php
+++ resources/views/web/pages/index.blade.php
@@ -1,11 +1,11 @@
 @extends('web.layout.app')
 @section('content')
 @include('web.partials.about')
-<x-card uri="https://hausermeier.ch" website="hausermeier.ch" description="Portfolio Webseite für das Architekten-Duo Martina Hauser und Sabine Meier." image="hausermeier.jpg" client="Hauser Meier Architektinnen, Zürich" design="Bivgrafik GmbH, Zürich" year="2020" />
+<x-card uri="https://hausermeier.ch" website="hausermeier.ch" description="Portfolio Webseite für das Architektinnen-Duo Martina Hauser und Sabine Meier." image="hausermeier.jpg" client="Hauser Meier Architektinnen, Zürich" design="Bivgrafik GmbH, Zürich" year="2020" />
 <x-card uri="https://day.ch" website="day.ch" description="Corporate website for the shoe manufacturer Day in Zurich. Online store is coming soon." image="day.jpg" client="Day AG, Zürich" design="WBG AG – Visuelle Kommunikation, Zürich" year="2020" />
-<x-card uri="https://sipt.ch" website="sipt.ch" description="Kursverwaltung für das Schweizer Institut für Psychotraumatologie." image="sipt.jpg" client="Schweizer Institut für Psychotraumatologie, Winterthur" design="Alexandra Noth, Winterthur" year="2020" />
+<x-card uri="https://sipt.ch" website="sipt.ch" description="Webseite und Kursverwaltung für Studenten und Dozenten im Auftrag des Schweizer Instituts für Psychotraumatologie in Winterthur." image="sipt.jpg" client="Schweizer Institut für Psychotraumatologie, Winterthur" design="Alexandra Noth, Winterthur" year="2020" />
 <x-card uri="https://aas.aero" website="aas.aero" description="Landingpage für Airline Assistance Switzerland." image="aas-aero.jpg" client="AAS, Switzerland" design="WBG AG – Visuelle Kommunikation, Zürich" year="2020" />
-<x-card uri="https://sajo.ch" website="sajo.ch" description="Online-Shop und Portfolio des Grafikers und Künstlers Samuel Jordi." image="sajo.jpg" client="Samuel Jordi, Winterthur" design="Samuel Jordi, Winterthur" year="2020" />
+<x-card uri="https://sajo.ch" website="sajo.ch" description="Online-Shop und Portfolio des Illustrators und Künstlers Samuel Jordi." image="sajo.jpg" client="Samuel Jordi, Winterthur" design="Samuel Jordi, Winterthur" year="2020" />
 <x-card uri="https://oxid-architektur.ch" website="oxid-architektur.ch" description="Portfolio Webseite für das Zürcher Architekturbüro Oxid." image="oxid-architektur.jpg" client="Oxid Architektur GmbH, Zürich" design="Bivgrafik GmbH, Zürich" year="2020" />
 <x-card uri="https://mobilecharge.ch" website="mobilecharge.ch" description="Konfigurator für eMobility Ladestationen." image="mobilecharge.jpg" client="Ferratec AG, Rudolfstetten" design="Jam'on digital AG, Zürich" year="2020" />
 <x-card uri="https://wbg.ch" website="wbg.ch" description="Portfolio Webseite für WBG, Zürich." image="wbg.jpg" client="WBG AG – Visuelle Kommunikation, Zürich" design="WBG AG – Visuelle Kommunikation, Zürich" year="2020" />
