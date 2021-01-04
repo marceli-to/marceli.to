b6643f5940b3c3acc4426b16380127ff3e1988e9
Marcel Stadelmann <marcel@jamon.digital>
2021-01-04 10:33:53 +0100

    add tracking code

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -1,5 +1,12 @@
 <footer class="site-footer">made with ❤️ by marceli.to</footer>
 <script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142689384-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-142689384-1');
+</script>
 </body>
 <!-- made with ❤ by marceli.to -->
 </html>
