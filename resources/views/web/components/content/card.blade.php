1ac3dd20b6462fd3deac50527be835a24eda861e
Marcel Stadelmann <marcel@jamon.digital>
2021-01-06 23:32:29 +0100

    add webp formats

--- resources/views/web/components/content/card.blade.php
+++ resources/views/web/components/content/card.blade.php
@@ -1,9 +1,12 @@
 <section class="post">
   <a href="{{$uri}}" target="_blank" rel="noreferrer" title="{{$website}} besuchen" class="card">
     <picture>
-      <source media="(min-width: 1200px)" srcset="/assets/img/projects/{{$image}}-lg.jpg" class="lazyload">
-      <source media="(min-width: 768px)" srcset="/assets/img/projects/{{$image}}-md.jpg" class="lazyload">
-      <source srcset="/assets/img/projects/{{$image}}-sm.jpg" class="lazyload">
+      <source media="(min-width: 1200px)" srcset="/assets/img/projects/{{$image}}-lg.webp" type="image/webp">
+      <source media="(min-width: 1200px)" srcset="/assets/img/projects/{{$image}}-lg.jpg">        
+      <source media="(min-width: 768px)" srcset="/assets/img/projects/{{$image}}-md.webp" type="image/webp">
+      <source media="(min-width: 768px)" srcset="/assets/img/projects/{{$image}}-md.jpg">
+      <source srcset="/assets/img/projects/{{$image}}-sm.webp" type="image/webp">
+      <source srcset="/assets/img/projects/{{$image}}-sm.jpg">
       <img src="/assets/img/projects/{{$image}}-sm.jpg" alt="{{$client}}" title="{{$client}}" class="is-responsive lazyload">
     </picture>
     <div class="card__content">
