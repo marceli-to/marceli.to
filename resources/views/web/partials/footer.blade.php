bfdef0215cfb6774177b03b72a2d730370999dbb
Marcel Stadelmann <m@marceli.to>
2021-06-22 15:27:19 +0200

    update ga

--- resources/views/web/partials/footer.blade.php
+++ resources/views/web/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="site-footer">made with&nbsp;&#10084;️&nbsp;by marceli.to</footer>
 <script src="{{ asset('assets/js/app.js') }}" type="text/javascript"></script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-142689384-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JHC31BJ7B7"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-142689384-1');
+  gtag('config', 'G-JHC31BJ7B7');
 </script>
 </body>
 <!-- made with ❤ by marceli.to -->
