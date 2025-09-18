633f4bfce2720477d0031d0bf3c0d61dd0449654
Marcel Stadelmann <m@marceli.to>
2025-09-18 11:52:09 +0200

    adds wiretap

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use MarceliTo\Wiretap\Facades\Wiretap;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -24,7 +25,15 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+          Wiretap::error('Exception occurred', [
+            'message' => $e->getMessage(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'url' => request()->fullUrl(),
+            'method' => request()->method(),
+            'user_id' => auth()->id(),
+            'ip' => request()->ip()
+        ]);
         });
     }
 }
